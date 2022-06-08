98219d8a75fb111363e95f83ada66ecfa8492723
G <frolovgeorgiy@gmail.com>
2022-06-09 02:16:08 +0300

    requirements + back sprites update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+//Highlight current block
 function getCurrentBlock() {
   let ans;
-  url = 'http://cors-anywhere.herokuapp.com/http://185.177.219.170:3001/api/getblockcount'
+  url = 'http://185.177.219.170:3001/api/getblockcount'
   return fetch(url).then(function(response) {
       response.text().then(function(text) {
+        let blockCounter = document.querySelector('.reward .wrapper .description p b')
         let rows = document.querySelectorAll('.table .row.default');
+        blockCounter.textContent = text;
         for (let row = 0; row < rows.length; row++) {
           if (parseInt(rows[row].firstElementChild.firstElementChild.textContent) > text) {
             rows[row - 1].classList.add('current');
@@ -17,3 +20,12 @@ function getCurrentBlock() {
 }
 
 document.onload = getCurrentBlock();
+
+
+//Open specification table
+let limiter = document.querySelector('.limiter')
+function openSpecificationTable() {
+  this.parentElement.parentElement.parentElement.classList.remove('disabled');
+}
+
+limiter.addEventListener('click', openSpecificationTable);
