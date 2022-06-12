dbb90c48fbe4ee7b3824fd9908ee7776040d2e45
G <frolovgeorgiy@gmail.com>
2022-06-12 19:58:05 +0300

    alpha

--- js/main.js
+++ js/main.js
@@ -29,3 +29,47 @@ function OpenSpecificationTable() {
 }
 
 limiter.addEventListener('click', OpenSpecificationTable);
+
+//Mobile menu
+
+const menuOpenBtn = document.querySelector("img.mobile_nav_button");
+const menuCloseBtn = document.querySelector(".mobile_menu p");
+const mobileMenu = document.querySelector(".mobile_menu");
+const mobileMenuLinks = document.querySelectorAll(".mobile_menu nav a");
+
+menuOpenBtn.onclick = () => {
+    menuOpen();
+}
+
+menuCloseBtn.onclick = () => {
+    menuClose();
+}
+
+function menuOpen() {
+    mobileMenu.classList.add("active");
+    mobileMenu.classList.remove("closed");
+    disableScroll();
+}
+
+function menuClose() {
+    mobileMenu.classList.remove("active");
+    mobileMenu.classList.add("closed");
+    enableScroll();
+}
+
+mobileMenuLinks.forEach((item) => {
+    item.onclick = () => {menuClose();}
+});
+
+function disableScroll() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
